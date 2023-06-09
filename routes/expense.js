9e064553b233c5e71752314faaad13f633ea6c6e
ahmar35 <muhammadahmar35@gmail.com>
2023-06-09 15:42:58 +0530

    routes update

--- routes/expense.js
+++ routes/expense.js
@@ -6,7 +6,6 @@ const auth=require('../middleware/auth')
 const routers=express.Router()
 
 routers.post('/ExpenseForm/',auth.authenticate,controllers.postExpenseDetails)
-routers.get('/ExpenseDetails',auth.authenticate,controllers.getExpenseDetails)
 routers.get('/user/download',auth.authenticate,controllers.getdownloadExpenseInfo)
 routers.delete('/DeleteInfo/:id',auth.authenticate,controllers.deleteExpenseInfo)
 routers.get('/expenses/page/:page/:currentExpensesPerRow',auth.authenticate,controllers.getExpenses)
